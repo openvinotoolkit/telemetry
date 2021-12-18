89737ef6bb8283663f3e91182e11a743ed2779c4
Anastasia Popova <anastasia.popova@intel.com>
2021-12-18 16:45:33 +0300

    Debug output removed.

--- src/opt_in_out.py
+++ src/opt_in_out.py
@@ -20,8 +20,7 @@ def main():
         print('Specify either "--opt_in" or "--opt_out" command line parameter.')
         exit(1)
 
-    tid = 'UA-186253784-1'
-    print('last version')
+    tid = None
     if args.opt_in:
         tm.Telemetry.opt_in(tid=tid)
     else:
