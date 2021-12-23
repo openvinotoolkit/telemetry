7d853f29c305ced6b1024a767ae64419ce7eb98d
Anastasia Popova <anastasia.popova@intel.com>
2021-12-23 17:30:01 +0300

    Returned base ID.

--- src/opt_in_out.py
+++ src/opt_in_out.py
@@ -20,7 +20,7 @@ def main():
         print('Specify either "--opt_in" or "--opt_out" command line parameter.')
         exit(1)
 
-    tid = None
+    tid = "UA-17808594-29"
     if args.opt_in:
         tm.Telemetry.opt_in(tid=tid)
     else:
