04c72b0df98110ab5c3c2479dbdd4d7d5bbb8907
Anastasiia Pnevskaia <anastasiia.pnevskaia@intel.com>
2023-10-06 11:34:41 +0200

    Check bandit job.

--- src/backend/backend_ga4.py
+++ src/backend/backend_ga4.py
@@ -41,7 +41,7 @@ class GA4Backend(TelemetryBackend):
             else:
                 raise ValueError("Incorrect backend URL.")
 
-            request.urlopen(req) #nosec
+            request.urlopen(req)
         except Exception as err:
             log.warning("Failed to send event with the following error: {}".format(err))
 
